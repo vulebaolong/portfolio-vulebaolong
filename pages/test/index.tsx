ad40deead6a0ebf63b51ab1e8fe2853acdd21317
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2023-09-08 13:16:44 +0700

    add page test

---
+++ pages/test/index.tsx
@@ -0,0 +1,10 @@
+import { Example } from "@/components/Example";
+
+function test() {
+    return (
+        <div className="example-container">
+            <Example />
+        </div>
+    );
+}
+export default test;
