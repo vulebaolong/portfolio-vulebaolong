4af6d0b98e9b4ca10ab3acb9109bab82b2f7becd
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2023-09-08 16:36:07 +0700

    fix link ProjectsBtn

--- pages/Home/ProjectsBtn.tsx
+++ pages/Home/ProjectsBtn.tsx
@@ -12,7 +12,7 @@ function ProjectsBtn() {
     return (
         <div className={`mx-auto ${dtBtn}`}>
             <Link
-                href={"/work"}
+                href={"/project"}
                 className="relative sm:p-5 p-3 w-[120px] h-[120px] sm:w-[185px] sm:h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-center bg-no-repeat group"
             >
                 <Image src={"/rounded-text.png"} width={141} height={148} alt="rounded-text" className="animate-spin-slow " priority={true}/>
